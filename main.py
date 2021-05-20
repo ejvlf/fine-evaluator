835fe1d3b94c2998832c9c62e681d4bb4a8604ab
Emanuel Ferreira <emanuel.ferreira@buildium.com>
2021-05-20 20:03:45 +0100

    fin

---
+++ main.py
@@ -0,0 +1,13 @@
+from objs.document import FineDocument
+
+def run ():
+    #instantiate parser
+
+    fine = FineDocument.from_pdf_file("test_fine.pdf")
+
+
+    
+
+if __name__ == "__main__":
+
+    run()
---
+++ objs/document.py
@@ -0,0 +1,52 @@
+from io import BytesIO, StringIO
+import os
+from pathlib import Path
+
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+class FineDocument:
+
+    @classmethod
+    def from_pdf_file (cls, filename :str, dir=None):
+       
+        output_string = StringIO()
+
+        if dir is not None:
+
+            workdir = dir
+
+        else:
+            os.chdir(os.path.dirname(__file__))
+            root_dir = Path(Path.cwd().parents[0])
+            workfile_dir = root_dir.joinpath(Path("docs")).joinpath(filename)
+
+        
+        with workfile_dir.open("rb") as fp:
+
+            parser = PDFParser(fp)
+            document = PDFDocument(parser)
+            rsrcmgr = PDFResourceManager()
+            device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            for page in PDFPage.create_pages(document):
+                interpreter.process_page(page)
+            
+            print(output_string.getvalue())
+
+        return cls, output_string
+
+
+
+        return cls, workdir
+
+    def __init__(self, file : StringIO):
+
+        pass
+
+    def __parse_pdf_for_pattern (pattern:str, file:bytes):
+        pass
